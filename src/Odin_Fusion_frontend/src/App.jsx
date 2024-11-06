e2b2a41b7be650380855a9e8cb07b191c4610c85
Sukuna <ajeemmanuel221@gmail.com>
2024-11-06 22:23:20 +0100

    cleaned the code

--- src/Odin_Fusion_frontend/src/App.jsx
+++ src/Odin_Fusion_frontend/src/App.jsx
@@ -2,28 +2,20 @@ import { useState } from 'react';
 import { Odin_Fusion_backend } from 'declarations/Odin_Fusion_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  // const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    Odin_Fusion_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   Odin_Fusion_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <h1>Odin Fusion Clean Table</h1>
     </main>
   );
 }
