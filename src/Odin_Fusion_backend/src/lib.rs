d8de5e7aad328eed47d00e0b02b1da084be7cb23
Sukuna <ajeemmanuel221@gmail.com>
2024-11-10 14:38:45 +0100

    added the bitcoin basic to cargo

--- src/Odin_Fusion_backend/src/lib.rs
+++ src/Odin_Fusion_backend/src/lib.rs
@@ -1,5 +1,40 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use crate::{bitcoin_api, ecdsa_api};
+use bitcoin::{
+    consensus::serialize,
+    hashes::Hash,
+    script::{Builder, PushBytesBuf},
+    sighash::{EcdsaSighashType, SighashCache},
+    Address, AddressType, PublicKey, Transaction, Txid,
+};
+use ic_cdk::api::management_canister::bitcoin::{
+    BitcoinNetwork, MillisatoshiPerByte, Satoshi, Utxo,
+};
+use ic_cdk::print;
+use std::convert::TryFrom;
+use std::str::FromStr;
+
+use super::common::transform_network;
+
+
+/// Returns the P2PKH address of this canister at the given derivation path.
+pub async fn get_p2pkh_address(
+    network: BitcoinNetwork,
+    key_name: String,
+    derivation_path: Vec<Vec<u8>>,
+) -> String {
+    // Fetch the public key of the given derivation path.
+    let public_key = ecdsa_api::ecdsa_public_key(key_name, derivation_path).await;
+
+    // Compute the address.
+    public_key_to_p2pkh_address(network, &public_key)
+}
+
+// Converts a public key to a P2PKH address.
+fn public_key_to_p2pkh_address(network: BitcoinNetwork, public_key: &[u8]) -> String {
+    Address::p2pkh(
+        &PublicKey::from_slice(public_key).expect("failed to parse public key"),
+        transform_network(network),
+    )
+    .to_string()
 }
 
